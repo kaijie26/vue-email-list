531c77dd71ba64b3899090c4129774c52440b4b4
kaijie26 <chenkaijie26@gmail.com>
2022-07-04 16:46:13 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -5,27 +5,28 @@
 var app = new Vue(
     {
         el: '#root',
+        
         data: {
             randomEmails: [],
 
 
         },
-        methods: {
-
-        },
 
         mounted() {
+            // Reperisco 10 volte l'email e qual volta singolarmente li pusho nel array
             for (let i = 0; i < 10; i++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+
                 .then((response) => {
-                this.randomEmails.push(response.data.response)
+
+                this.randomEmails.push(response.data.response);
                 
             });
                 
             };
 
 
-        },
+        }
 
     }
 )
